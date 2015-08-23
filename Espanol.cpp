f24beda1dce34bd8f36a79e2a402c16ea72ebd0b
misterdanb <danb@hasi.it>
2015-08-23 13:45:40 +0200

    Initial commit

---
+++ Espanol.cpp
@@ -0,0 +1,150 @@
+#include "Espanol.h"
+
+Espanol::Espanol(char* ssid, char* password, char* broker, int port, void (*callback)(char*, uint8_t*, unsigned int))
+    : _debug(false),
+      _ssid(ssid),
+      _password(password),
+      _broker(broker),
+      _port(port),
+      _topics({ 0, }),
+      _client(),
+      _mqtt(broker, port, callback, _client)
+{
+}
+
+void Espanol::setDebug(boolean debug)
+{
+    _debug = debug;
+}
+
+boolean Espanol::getDebug()
+{
+    return _debug;
+}
+
+void Espanol::loop()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        connectWiFi();
+    }
+
+    if (!_mqtt.connected())
+    {
+        connectMQTT();
+    }
+
+    _mqtt.loop();
+}
+
+boolean Espanol::connected()
+{
+    return (WiFi.status() == WL_CONNECTED) && _mqtt.connected();
+}
+
+void Espanol::subscribe(char* newTopic)
+{
+    for (char* topic : _topics)
+    {
+        if (topic != 0)
+        {
+            if (strcmp(topic, newTopic) == 0)
+            {
+                return;
+            }
+        }
+    }
+
+    for (char*& topic : _topics)
+    {
+        if (topic == 0)
+        {
+            topic = newTopic;
+            _mqtt.subscribe(newTopic);
+
+            return;
+        }
+    }
+}
+
+void Espanol::unsubscribe(char* currentTopic)
+{
+    for (char*& topic : _topics)
+    {
+        if (strcmp(topic, currentTopic) == 0)
+        {
+            _mqtt.unsubscribe(currentTopic);
+            topic = 0;
+
+            return;
+        }
+    }
+}
+
+void Espanol::connectWiFi()
+{
+    if (_debug)
+    {
+        Serial.print("Connecting to ");
+        Serial.println(_ssid);
+    }
+
+    WiFi.begin(_ssid, _password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+
+        if (_debug)
+        {
+            Serial.print(".");
+        }
+    }
+
+    if (_debug)
+    {
+        Serial.println();
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+void Espanol::connectMQTT()
+{
+    String clientName;
+    clientName += "esp8266-";
+    clientName += String(micros() & 0xffff, 16);
+
+    char* clientNameCharArray = new char[clientName.length() + 1];
+    clientName.toCharArray(clientNameCharArray, clientName.length());
+
+    if (_debug)
+    {
+        Serial.print("Connecting to broker");
+    }
+
+    while (!_mqtt.connect(clientNameCharArray))
+    {
+        delay(500);
+
+        if (_debug)
+        {
+            Serial.print(".");
+        }
+    }
+
+    if (_debug)
+    {
+        Serial.println();
+        Serial.println("Connected to MQTT broker");
+    }
+
+    for (char* topic : _topics)
+    {
+        if (topic != 0)
+        {
+            _mqtt.subscribe(topic);
+        }
+    }
+}
---
+++ Espanol.h
@@ -0,0 +1,48 @@
+/*
+ Espanol.h - A esp8266 arduino library establishing a
+             wifi and mqtt connection and keeping it up.
+  danb
+  http://hasi.it
+*/
+
+#ifndef Espanol_h
+#define Espanol_h
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <stdint.h>
+#include <PubSubClient.h>
+
+class Espanol
+{
+public:
+    Espanol(char*, char*, char*, int, void (*)(char*, uint8_t*, unsigned int));
+
+    void setDebug(boolean);
+    boolean getDebug();
+
+    void loop();
+    boolean connected();
+
+    void subscribe(char*);
+    void unsubscribe(char*);
+    void publish(char*, char*);
+
+private:
+    void connectWiFi();
+    void connectMQTT();
+
+private:
+    boolean _debug;
+    char* _ssid;
+    char* _password;
+    char* _broker;
+    int _port;
+    char* _topics[20];
+    WiFiClient _client;
+    PubSubClient _mqtt;
+};
+
+#endif
